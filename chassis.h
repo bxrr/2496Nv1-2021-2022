c2d268adee438aba1d4e0989ca164a93bb369be8
bxrr <68095440+bxrr@users.noreply.github.com>
2021-07-25 07:59:17 -0700

    hello t
    fixed a bunch of stuff

--- chassis.h
+++ chassis.h
@@ -1,16 +1,26 @@
+#include "main.h"
 #include "port_defines.h"
 
 class Chassis
 {
 private:
-  pros::Motor backLeft(BACK_LEFT_PORT, E_MOTOR_GEARSET_6, false);
-	pros::Motor midLeft(MID_LEFT_PORT, E_MOTOR_GEARSET_6, false);
-	pros::Motor frontLeft(FRONT_LEFT_PORT, E_MOTOR_GEARSET_6, false);
-	pros::Motor backRight(BACK_RIGHT_PORT, E_MOTOR_GEARSET_6, false);
-	pros::Motor midRight(MID_RIGHT_PORT, E_MOTOR_GEARSET_6, false);
-	pros::Motor frontRight(FRONT_RIGHT_PORT, E_MOTOR_GEARSET_6, false);
+  pros::Motor backLeft;
+	pros::Motor midLeft;
+	pros::Motor frontLeft;
+	pros::Motor backRight;
+	pros::Motor midRight;
+	pros::Motor frontRight;
 
 public:
+  Chassis() : backLeft(BACK_LEFT_PORT, pros::E_MOTOR_GEARSET_06, false),
+              midLeft(MID_LEFT_PORT, pros::E_MOTOR_GEARSET_06, false),
+              frontLeft(FRONT_LEFT_PORT, pros::E_MOTOR_GEARSET_06, false),
+              backRight(BACK_RIGHT_PORT, pros::E_MOTOR_GEARSET_06, false),
+              midRight(MID_RIGHT_PORT, pros::E_MOTOR_GEARSET_06, false),
+              frontRight(FRONT_RIGHT_PORT, pros::E_MOTOR_GEARSET_06, false)
+              {}
+
+  // General spin functions ====================================================
   enum unitType {PCT, VOLT};
   void spinLeft(double speed, unitType unit=VOLT) // VOLT is -127 to 127, PCT is -100 to 100
   {
@@ -44,25 +54,28 @@ public:
      }
   }
 
+  // Change brake type =========================================================
   void brakeHold()
   {
-     backLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD)
-     midLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD)
-     frontLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD)
-     backRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD)
-     midRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD)
-     frontRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD)
+     backLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+     midLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+     frontLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+     backRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+     midRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+     frontRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
   }
 
   void brakeCoast()
   {
-    backLeft.set_brake_mode(pros::E_MOTOR_BRAKE_COAST)
-    midLeft.set_brake_mode(pros::E_MOTOR_BRAKE_COAST)
-    frontLeft.set_brake_mode(pros::E_MOTOR_BRAKE_COAST)
-    backRight.set_brake_mode(pros::E_MOTOR_BRAKE_COAST)
-    midRight.set_brake_mode(pros::E_MOTOR_BRAKE_COAST)
-    frontRight.set_brake_mode(pros::E_MOTOR_BRAKE_COAST)
+    backLeft.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    midLeft.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    frontLeft.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    backRight.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    midRight.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    frontRight.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
   }
+
+  // Motor position functions ==================================================
 
   double getLeftPos() // Returns the current average encoding of all the left chassis motors
   {
@@ -77,4 +90,4 @@ public:
              midRight.get_position() +
              frontRight.get_position()) / 3;
   }
-}
+};
--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 
 #include "port_defines.h"
 
+Chassis chas;
+pros::Imu inert(IMU_PORT);
+pros::Controller con(pros::E_CONTROLLER_MASTER);
+
 void initialize()
 {
-	pros::Controller con(E_CONTROLLER_MASTER);
-	Chassis chas;
-	pros::Imu inert(IMU_PORT);
 	inert.reset();
 }
 
@@ -20,13 +21,13 @@ void initialize()
      int leftStartPos = chas.getLeftPos();
      int rightStartPos = chas.getRightPos();
 
-		 // Distance error (only P)
-		 double distError
+		 // Distance error: motor encoding + P
+		 double distError;
 		 double currentPos = 0.0;
 		 float distKp = 0.2;
 
-		 // Rotational Error - intertial (PID)
-		 double initialRotation = inert.getRotation();
+		 // Drive straight stuff: intertial + PID
+		 double initialRotation = inert.get_rotation();
      double lastError, derivative;
      double error = 0.0;
      double integral = 0.0;
@@ -35,19 +36,21 @@ void initialize()
 		 float kI = 0.07;
 		 float kD = 0.03;
 
+		 // Drive loop, might add a timeout thing if it's needed
      while(currentPos < targetPos - 5)
      {
-			 distError = targetPos - ((leftStarPos - chas.getLeftPos()) + (rightStartPos - chas.getRightPos()) / 2;
+			 distError = targetPos - ((leftStartPos - chas.getLeftPos()) + (rightStartPos - chas.getRightPos()) / 2);
 
 			 lastError = error;
-			 error = initialRotation - inert.getRotation();
+			 error = initialRotation - inert.get_rotation();
 			 integral += error;
 			 derivative = error - lastError;
 
-			 rightSpeed = distError * distKp;
-			 leftSpeed = rightSpeed + (error * kP) + (integral * kI) + (derivative * kD);
+			 double rightSpeed = distError * distKp;
+			 double leftSpeed = rightSpeed + (error * kP) + (integral * kI) + (derivative * kD);
 
-			 chas.spinLeft(leftSpeed)
+			 chas.spinLeft(leftSpeed);
+			 chas.spinRight(rightSpeed);
      }
  }
 
@@ -56,23 +59,24 @@ void initialize()
  {
 
  }
+
 // main auton function
 void autonomous()
 {
-	while(inert.is_calibrating())
+	while(inert.is_calibrating()) // Check if the intertial sensor is calibrating before doing anything
 	{
-		pros:delay(5);
+		pros::delay(5);
 	}
 }
 
 // driver control functions ====================================================
 void arcadeDrive()
 {
-    if(abs(con.get_analog(E_CONTROLLER_ANALOG_LEFT_Y)) > 3 || abs(con.get_analog(E_CONTROLLER_ANALOG_RIGHT_X)) > 3)
+    if(abs(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y)) > 3 || abs(con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X)) > 3)
     {
 				chas.brakeCoast();
-        chas.spinLeft(con.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) + con.get_analog(E_CONTROLLER_ANALOG_RIGHT_X) * 0.5);
-        chas.spinRight(con.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) + con.get_analog(E_CONTROLLER_ANALOG_RIGHT_X) * 0.5);
+        chas.spinLeft(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) + con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X) * 0.5);
+        chas.spinRight(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) + con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X) * 0.5);
     }
     else
     {
@@ -82,11 +86,11 @@ void arcadeDrive()
 
 void tankDrive()
 {
-    if(abs(con.get_analog(E_CONTROPLLER_ANALOG_LEFT_Y)) > 3 || abs(con.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y)) > 3)
+    if(abs(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y)) > 3 || abs(con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)) > 3)
     {
 			  chas.brakeCoast();
-        chas.spinLeft(con.get_analog(E_CONTROPLLER_ANALOG_LEFT_Y));
-        chas.spinRight(con.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y));
+        chas.spinLeft(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+        chas.spinRight(con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
     }
     else
     {
@@ -96,12 +100,12 @@ void tankDrive()
 
 // main function
 void opcontrol() {
-	while(inert.is_calibrating())
+	while(inert.is_calibrating())  // Check if the intertial sensor is calibrating before doing anything
 	{
-		pros:delay(5);
+		pros::delay(5);
 	}
 
-	while (true) {
+	while (true) { // Drive loop (there's an arcadeDrive() function and tankDrive() function.
 		arcadeDrive();
 	}
 }
